507fd13cfb41d03555f1506288b844e08e905f39
Sosnus <sosnus15@gmail.com>
2020-10-30 11:15:34 +0100

    add run and connect script run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# NOTE: change volume path, >>>> -v /<host pwd>:/data <<<<
+
+# run interactive, detashed container
+docker run -d -it --name ros -v /data/dockervolumes/ros:/data marsyard_leo
+
+# connect to shell in container
+docker exec -it ros bash  
