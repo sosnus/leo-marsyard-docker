7b7c85a3d3efeec59c1cec07afb101f8d7edb0e6
Ubuntu <sms-admin@sms-pallene.s0kf3j1octjedkiexudbontl3a.ax.internal.cloudapp.net>
2020-12-07 17:18:31 +0000

    new dockerfile, new basic image

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 build() {
-  echo "Building Leo"
-  docker build -f Dockerfile.leo --tag marsyard_leo --no-cache .
+  echo "Building EmulatorBot"
+  docker build -f Dockerfile --tag emulatorbot --no-cache .
 }
 
 build
